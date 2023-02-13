75349718bff3fd75b396f048c9649cbb3061f0db
Mohiuddinr22 <88410867+Mohiuddinr22@users.noreply.github.com>
2023-02-13 22:40:19 +0600

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask1__13_02_2023_
+{
+    struct CustomDate 
+    {
+        public byte day;
+        public byte month;
+        public ushort year;
+
+        public CustomDate(byte day, byte month, ushort year)
+        {
+                this.day = day;
+                this.month = month;
+                this.year = year;
+        }
+
+        public void ShowDate()
+        {
+            Console.WriteLine("{0}/{1}/{2}",this.day,this.month,this.year);
+        }
+
+    }
+    class Product
+    {
+        private uint id;
+        private string name;
+        private CustomDate manufactureDate;
+        private double price;
+
+        public uint GetId()
+        {
+            return this.id;
+        }
+        public string GetName() 
+        {
+            return this.name;
+        }
+        public double GetPrice() 
+        {
+            return this.price;  
+        }
+        public CustomDate GetManufactureDate() 
+        {
+            return this.manufactureDate;
+        }
+
+        public void SetId(uint id)
+        {
+            this.id = id;
+        }
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public void SetManufactureDate(CustomDate manufactureDate)
+        {
+            this.manufactureDate = manufactureDate;
+        }
+        public void SetPrice(double price) 
+        {
+            this.price = price;
+        }
+        public Product(){}
+
+        public Product(uint id, string name, CustomDate manufactureDate, double price)
+        {
+            this.SetId(id);
+            this.SetName(name);
+            this.SetManufactureDate(manufactureDate);
+            this.SetPrice(price);
+        }
+        public void ShowProductDetails()
+        {
+            Console.WriteLine("ID               : {0}", this.GetId());
+            Console.WriteLine("Name             : {0}", this.GetName());
+            Console.Write    ("Manufacture Date : ");
+            this.GetManufactureDate().ShowDate();
+            Console.WriteLine("Price            : ${0}", this.GetPrice());
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask1__13_02_2023_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomDate manufactureDate1 = new CustomDate(10,02,2022);
+            
+            Product Television = new Product(8899, "Samsung Series 22", manufactureDate1, 5599.99);
+            
+            Console.WriteLine("{0} details : ", Television.GetName());
+            Television.ShowProductDetails();
+
+            //---------------------------------------------------------//
+
+            CustomDate manufactureDate2 = new CustomDate(16, 6, 2021);
+            
+            Product Refrigerator = new Product();
+            Refrigerator.SetId(1118);
+            Refrigerator.SetName("Shark XX88");
+            Refrigerator.SetManufactureDate(manufactureDate2);
+            Refrigerator.SetPrice(999.899);
+            
+            Console.WriteLine("{0} details : ", Refrigerator.GetName());
+            Refrigerator.ShowProductDetails();
+        }
+    }
+}
